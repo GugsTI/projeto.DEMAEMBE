fe901226dae105e6cf2b926ab9a0a42d0166f144
aa <0>
2023-08-08 11:58:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img class="img_logo" src="glow 1 (1).png" alt="d"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">serviços</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">contato</a>
+        </li>    
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="titulo">
+<h1>Os pisos mais vendidos!</h1>
+
+</div>
+
+
+
+
+</template>
+
+<style scoped>
+.img_logo{
+height: 110px;
+
+  
+  
+}
+
+.navbar{
+  background: linear-gradient(180deg, #3B48BE 0%, rgba(59, 72, 190, 0.65) 97.12%, rgba(59, 72, 190, 0.00) 100%);}
+
+  .titulo{
+    color: #000;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+font-family: Inter;
+font-size: 40px;
+font-style:normal;
+font-weight: 900;
+line-height:normal;
+
+  }
+
+</style>
